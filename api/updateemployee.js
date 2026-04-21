da8932b265c36fef31cc5b1206a925adb8a5e1e0
acces-cible <info@lesadaptationsacces-cible.com>
2026-04-20 21:33:54 -0400

    Update updateemployee.js

--- api/updateemployee.js
+++ api/updateemployee.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
-export default async function handler(req, res) {
+// POST /api/updateEmployee
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
