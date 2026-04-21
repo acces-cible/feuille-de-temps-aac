9b0c6b50c6e1db6b27e8eb7fd7ad40bd53786819
acces-cible <info@lesadaptationsacces-cible.com>
2026-04-20 21:34:08 -0400

    Update syncairtable.js

--- api/syncairtable.js
+++ api/syncairtable.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 // POST /api/syncairtable
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
