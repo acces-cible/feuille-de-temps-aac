984248e99e22c1d195b628070a53826ac3b60733
acces-cible <info@lesadaptationsacces-cible.com>
2026-04-20 21:34:43 -0400

    Update getemployees.js

--- api/getemployees.js
+++ api/getemployees.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
