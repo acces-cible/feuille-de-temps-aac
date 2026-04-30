9dc3f555bc42452cef62245507a0d82fdd06c76b
acces-cible <info@lesadaptationsacces-cible.com>
2026-04-30 11:51:36 -0400

    Update updateemployee.js

--- api/updateemployee.js
+++ api/updateemployee.js
@@ -15,17 +15,20 @@ module.exports = async function handler(req, res) {
 
   const { AIRTABLE_TOKEN, AIRTABLE_BASE_ID } = process.env;
   const TABLE_NAME = encodeURIComponent("Employés");
+
   const headers = {
     Authorization: `Bearer ${AIRTABLE_TOKEN}`,
     'Content-Type': 'application/json'
   };
+
   const url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${TABLE_NAME}/${airtableId}`;
 
   const fields = {};
-  if (body.inputMode !== undefined) fields['Mode saisie']   = body.inputMode;
-  if (body.autoFill  !== undefined) fields['AutoFill']      = body.autoFill;
-  if (body.partTime  !== undefined) fields['Temps partiel'] = body.partTime;
-  if (body.password  !== undefined) fields['Mot de passe']  = body.password ?? '';
+  if (body.inputMode      !== undefined) fields['Mode saisie']     = body.inputMode;
+  if (body.autoFill       !== undefined) fields['AutoFill']        = body.autoFill;
+  if (body.partTime       !== undefined) fields['Temps partiel']   = body.partTime;
+  if (body.password       !== undefined) fields['Mot de passe']    = body.password ?? '';
+  if (body.reminderCount  !== undefined) fields['Compteur Rappel'] = body.reminderCount;
 
   if (Object.keys(fields).length === 0) {
     return res.status(200).json({ message: 'Rien à mettre à jour' });
